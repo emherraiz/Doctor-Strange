b62ee7c567b60c28efaf9a1e0fd5e17912d13bb5
Enrique Marina <eherraiz@myuax.com>
2022-06-20 23:01:20 +0200

    df

---
+++ Escenario_elegido.py
@@ -0,0 +1,10 @@
+from enum import Enum
+from lista_escenarios import escenarios
+'''
+class TipoEscenario(Enum):
+    sanctum_sanctorum = [10000, 10, 10, 100]
+    stark_tower = [20000, 20, 25, 200]
+    xavier_school = [80000, 30, 40, 300]
+'''
+print(escenarios)
+
---
+++ lista_escenarios.py
@@ -0,0 +1,1 @@
+escenarios = {'sanctum_sanctorum' : [10000, 10, 10, 100], 'stark_tower' : [20000, 20, 25, 200], 'xavier_school' : [80000, 30, 40, 300]}
