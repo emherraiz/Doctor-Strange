393804242ef0b8137d0c31be9fbe1e397586e7f9
Enrique Marina <eherraiz@myuax.com>
2022-06-21 10:24:55 +0200

    hacemos diccionario

--- lista_escenarios.py
+++ lista_escenarios.py
@@ -1,1 +1,7 @@
-escenarios = {'sanctum_sanctorum' : [10000, 10, 10, 100], 'stark_tower' : [20000, 20, 25, 200], 'xavier_school' : [80000, 30, 40, 300]}
+# Los escenarios tienen diferentes atributos....
+
+escenarios = {
+'sanctum_sanctorum' : [10000, 10, 10, 100],
+'stark_tower' : [20000, 20, 25, 200],
+'xavier_school' : [80000, 30, 40, 300]
+}
