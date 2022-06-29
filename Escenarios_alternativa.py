7330ffb2b0a8e449ea9c15b338be7682453a6594
Enrique Marina <eherraiz@myuax.com>
2022-06-29 20:53:51 +0200

    seguimos

---
+++ Escenarios_alternativa.py
@@ -0,0 +1,7 @@
+# Manera alternativa de hacerlo
+# El diccionario lo metería en otro fichero
+escenarios = {
+'sanctum_sanctorum' : [10000, 10, 10, 100],
+'stark_tower' : [20000, 20, 25, 200],
+'xavier_school' : [80000, 30, 40, 300]
+}
---
+++ Escenarios_disponibles.py
@@ -0,0 +1,8 @@
+from enum import Enum
+# Los escenarios tienen diferentes atributos....
+class TipoEscenario(Enum):
+    sanctum_sanctorum = [10000, 10, 10, 100]
+    stark_tower = [20000, 20, 25, 200]
+    xavier_school = [80000, 30, 40, 300]
+
+
